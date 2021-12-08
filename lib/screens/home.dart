a9b4c1fdf793a13c0be79ac1eb0b43a3f81abe1b
Jagal R Nath <jagalsree007@gmail.com>
2021-12-08 12:16:58 +0530

    update

---
+++ lib/screens/home.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Container(
+          height: MediaQuery.of(context).size.height * 0.5,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                children: [
+                  Image.asset('assets/image_data/sidemenu.png'),
+                  Text('Car Shoppee',
+                
+                  )
+                ],
+              )
+            ],
+          ),
+        )
+      ],
+    ));
+  }
+}
